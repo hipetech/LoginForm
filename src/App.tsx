c91ac3ccf66ac0cc7869e9faa3e7eefe4aaea802
hipetech <kirillko20002@gmail.com>
2024-11-28 16:37:45 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>
+        Hello world
+      </Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;

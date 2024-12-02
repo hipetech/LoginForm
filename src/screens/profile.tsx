5b2969ed22c2db1df973b31e816b1d1e738462d8
hipetech <kirillko20002@gmail.com>
2024-12-02 13:14:10 +0200

    code cleanups

--- src/screens/profile.tsx
+++ src/screens/profile.tsx
@@ -13,7 +13,6 @@ import { focusManager, useQuery } from '@tanstack/react-query';
 import { User } from '../types/User.ts';
 import { useIsFocused, useNavigation } from '@react-navigation/native';
 import { getAccessToken } from '../helpers/getAccessToken.ts';
-import { setAccessToken } from '../helpers/setAccessToken.ts';
 import { queryClient } from '../App.tsx';
 import { removeAccessToken } from '../helpers/removeAccessToken.ts';
 
@@ -62,14 +61,6 @@ export const Profile = () => {
         <ProfileHeader user={data} isLoading={isLoading} />
         <Button variant="secondary" onPress={logout}>
           Logout
-        </Button>
-        <Button
-          onPress={async () =>
-            await setAccessToken(
-              'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJlbWlseXMiLCJlbWFpbCI6ImVtaWx5LmpvaG5zb25AeC5kdW1teWpzb24uY29tIiwiZmlyc3ROYW1lIjoiRW1pbHkiLCJsYXN0TmFtZSI6IkpvaG5zb24iLCJnZW5kZXIiOiJmZW1hbGUiLCJpbWFnZSI6Imh0dHBzOi8vZHVtbXlqc29uLmNvbS9pY29uL2VtaWx5cy8xMjgiLCJpYXQiOjE3MzMwODQ2MzksImV4cCI6MTczMzA4ODIzOX0.51hUR0IcBBGOwwD5dBYApzN1bTiqSkF2EOqM08fsOzU',
-            )
-          }>
-          Set invalid token
         </Button>
       </View>
     </SafeAreaView>
